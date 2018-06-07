a43fa169a7833e2eb0c97ceef6e7c0c12ccd84a9
beatriz <beatriz@ckl.io>
2018-06-07 10:04:31 -0300

    Add facebook login integration

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,29 @@ import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class DefaultDocument extends Document {
   render() {
+    const FB_SCRIPT = `
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '240115020081725',
+          xfbml      : true,
+          version    : 'v3.0'
+        });
+        FB.AppEvents.logPageView();
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+      `
     return (
       <html lang="en">
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
+          <script dangerouslySetInnerHTML={{ __html: FB_SCRIPT }} />
         </Head>
         <body>
           <Main />
--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,18 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Button from '../../components/atoms/button'
 import InputWithLabel from '../../components/molecules/input-with-label'
 import PasswordField from '../../components/molecules/password-field'
 import classes from '../signin/styles.css'
+import FacebookLoginButton from '../../components/organisms/facebook-login-button'
 
-const signIn = () => (
-  <form>
-    <div className={classes.Login}>
-      <InputWithLabel label="Email" type="email" />
-      <PasswordField label="Senha" type="password" />
-      <Button label="Login" />
-    </div>
-  </form>
-)
+class signIn extends Component {
+  state = {
+    username: null,
+  }
+
+  onFacebookLogin = (loginStatus, resultObject) => {
+    if (loginStatus === true) {
+      this.setState({
+        username: resultObject.user.name,
+      })
+    } else {
+      alert('Facebook login error')
+    }
+  }
+
+  render() {
+    const { username } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React Social Media Login</h1>
+        </header>
+
+        <div className="App-intro">
+          {!username && (
+            <div>
+              <form>
+                <div className={classes.Login}>
+                  <InputWithLabel label="Email" type="email" />
+                  <PasswordField label="Senha" type="password" />
+                  <Button label="Login" />
+                </div>
+              </form>
+
+              <p>Click on one of any button below to login</p>
+              <FacebookLoginButton onLogin={this.onFacebookLogin}>
+                <button>Facebook</button>
+              </FacebookLoginButton>
+            </div>
+          )}
+          {username && <p>Welcome back, {username}</p>}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default signIn
