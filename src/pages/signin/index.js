86d103ac55c12dea5551a59b295018e44bb422af
beatriz <beatriz@ckl.io>
2018-06-11 14:29:07 -0300

    Implement login using react-fetches

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import GoogleLogin from 'react-google-login'
+import { connect } from 'react-fetches'
 
 import ButtonSignin from '../../components/atoms/button-signin'
 import InputWithLabel from '../../components/molecules/input-with-label'
@@ -7,9 +8,15 @@ import PasswordField from '../../components/molecules/password-field'
 import styles from '../signin/styles.css'
 import FacebookLoginButton from '../../components/organisms/facebook-login-button'
 
+const mapDispatchToProps = (http, dispatch) => ({
+  signInUser: dispatch(http.post('login')),
+})
+
 class signIn extends Component {
   state = {
+    loggedUser: {},
     username: null,
+    password: '',
   }
 
   onFacebookLogin = (loginStatus, resultObject) => {
@@ -33,7 +40,7 @@ class signIn extends Component {
   responseSuccessGoogle = response => {
     if (response.profileObj.name) {
       this.setState({
-        username: response.profileObj.name,
+        username: response.getBasicProfile().getName(),
       })
     } else {
       this.responseFailureGoogle(response)
@@ -44,19 +51,53 @@ class signIn extends Component {
     alert(`Google login error ${response}`)
   }
 
+  signInUser = event => {
+    event.preventDefault()
+    console.log('USER: ', this.state.username)
+    console.log('password: ', this.state.password)
+    this.props
+      .signInUser({
+        username: this.state.username,
+        password: this.state.password,
+      })
+      .then(({ data }) => {
+        console.log('password: ', data)
+        this.setState(prevState => ({
+          loggedUser: data,
+        }))
+      })
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    this.setState({ [inputIdentifier]: event.target.value })
+  }
+
   render() {
-    const { username } = this.state
+    const {
+      username,
+      loggedUser: { token },
+    } = this.state
     return (
       <div className={styles.siginWrapper}>
         <h1 className="signin-title">CKL Labs</h1>
 
         <div className="signin-content__fields">
-          {!username && (
+          {!token && (
             <div>
-              <form>
+              <form onSubmit={this.signInUser}>
                 <div className={styles.siginEmailField}>
-                  <InputWithLabel label="Email" type="email" />
-                  <PasswordField label="Senha" type="password" />
+                  <InputWithLabel
+                    label="Username"
+                    type="text"
+                    value={this.state.username}
+                    changed={event => this.inputChangedHandler(event, 'username')}
+                  />
+                  <PasswordField
+                    label="Senha"
+                    type="password"
+                    value={this.state.password}
+                    changed={event => this.inputChangedHandler(event, 'password')}
+                  />
                   <ButtonSignin label="Login" />
                 </div>
               </form>
@@ -73,7 +114,7 @@ class signIn extends Component {
               </div>
             </div>
           )}
-          {username && (
+          {token && (
             <div>
               <button onClick={this.onFacebookLogout}>facebook logout</button>
               <p>Welcome back, {username}</p>
@@ -85,4 +126,7 @@ class signIn extends Component {
   }
 }
 
-export default signIn
+export default connect(
+  null,
+  mapDispatchToProps
+)(signIn)
