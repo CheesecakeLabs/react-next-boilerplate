1a29ce78aef9fb8250fd46fd8d040fa79a905b70
beatriz <beatriz@ckl.io>
2018-06-21 17:24:55 -0300

    Create a basic map component

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 
 import logo from '_images/logo.png'
 
+import LeafletWrapper from '../../components/organisms/LeafletMap'
+
 import styles from './styles.css'
 
 const CKLBoilerplate = () => (
@@ -13,6 +15,7 @@ const CKLBoilerplate = () => (
     {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
     <marquee className={styles.text}>React Boilerplate</marquee>
     <hr />
+    <LeafletWrapper />
   </Fragment>
 )
 
