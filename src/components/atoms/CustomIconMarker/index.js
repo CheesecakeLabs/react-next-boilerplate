e8334c33a51c281c7fa7cd7b43b37e706946f371
beatriz <beatriz@ckl.io>
2018-06-26 16:21:14 -0300

    Adjusts custom icon marker

--- src/components/atoms/CustomIconMarker/index.js
+++ src/components/atoms/CustomIconMarker/index.js
@@ -1,16 +1,11 @@
-import markerIcon from '../../../images/icon.png'
+import markerIcon from '../../../images/marker-icon.png'
 import { isBrowser, Icon } from '../../../utils/LeafletElements'
 
 export const CustomIconMarker = () => {
   if (isBrowser) {
     return new Icon({
       iconUrl: markerIcon,
-      iconSize: [38, 46],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76],
-      shadowUrl: markerIcon,
-      shadowSize: [68, 46],
-      shadowAnchor: [22, 94],
+      iconSize: [35, 37],
     })
   }
   return null
