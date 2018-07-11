979dd346fb8c254c451e33c12b273377dcc6d03f
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-11 17:30:25 -0300

    refactor(components): Move Form component to organisms and rewrite it using class

---
+++ src/components/organisms/Form/index.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  onSubmit = ev => {
+    ev.preventDefault()
+    this.props.handleSubmit(ev)
+  }
+
+  render = () => {
+    const { className, children, responseTest } = this.props
+
+    return (
+      <form className={className} onSubmit={this.onSubmit}>
+        {children}
+        <input type="submit" value="submit"/>
+      </form>
+    )
+  }
+}
+
+const elementTypes = [PropTypes.element, PropTypes.arrayOf(PropTypes.element)]
+
+Form.propTypes = {
+  className: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType(elementTypes).isRequired,
+}
+
+Form.defaultProps = {
+  className: undefined,
+  handleSubmit: null,
+  children: null,
+}
+
+export default Form
