c53860f19abaf95b76c678d8228a686c907277e4
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-11 17:15:23 -0300

    feat(components): Add a form field component
    that is composed of a `label` passed as `children` prop, a `Input` component and a error message
    passed as `errorMessage` prop

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Input = ({ className, type, required, value, handleChange }) => (
+const Input = ({ className, type, id, name, required, value, handleChange }) => (
   <input
     className={className}
     type={type}
+    id={id}
+    name={name}
     required={required}
     value={value}
     onChange={handleChange}
@@ -14,6 +16,8 @@ const Input = ({ className, type, required, value, handleChange }) => (
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
+  id: PropTypes.string,
+  name: PropTypes.string,
   required: PropTypes.bool,
   value: PropTypes.string,
   handleChange: PropTypes.func.isRequired,
@@ -22,6 +26,8 @@ Input.propTypes = {
 Input.defaultProps = {
   className: undefined,
   type: 'text',
+  id: undefined,
+  name: undefined,
   required: false,
   value: '',
   handleChange: null,
