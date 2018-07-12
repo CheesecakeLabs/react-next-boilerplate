f1d361178274a1d8a084f32b2d7b4581f128673c
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-12 13:52:54 -0300

    feat(components): Add `multiline` prop to transform Input component in a textarea

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,23 +1,26 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Input = ({ className, type, id, name, required, value, handleChange }) => (
-  <input
-    className={className}
-    type={type}
-    id={id}
-    name={name}
-    required={required}
-    value={value}
-    onChange={handleChange}
-  />
-)
+const Input = ({ className, type, id, name, multiline, required, value, handleChange }) => {
+  const props = {
+    className,
+    id,
+    name,
+    required,
+    value,
+    onChange: handleChange
+  }
+  return multiline ?
+    <textarea {...props} /> :
+    <input {...props} type={type} />
+}
 
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
   id: PropTypes.string,
   name: PropTypes.string,
+  multiline: PropTypes.bool,
   required: PropTypes.bool,
   value: PropTypes.string,
   handleChange: PropTypes.func.isRequired,
@@ -28,6 +31,7 @@ Input.defaultProps = {
   type: 'text',
   id: undefined,
   name: undefined,
+  multiline: false,
   required: false,
   value: '',
   handleChange: null,
