2501b522e0abea2196117212620562e94729a652
pattyssilveira <pattyssilveira@gmail.com>
2018-07-13 16:15:54 -0300

    Feat: create button-icon component

---
+++ src/components/atoms/Button-icon/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import loginIcon from './login-icon.svg'
+import styles from './styles.css'
+
+const ButtonIcon = props => <img className="buttonIcon" src={loginIcon} alt="button-icon" />
+
+export default ButtonIcon
