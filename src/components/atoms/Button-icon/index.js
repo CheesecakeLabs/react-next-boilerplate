aad9820ddc1a11fdbd51df104e915950511946c0
pattyssilveira <pattyssilveira@gmail.com>
2018-07-13 18:03:01 -0300

    Refactor: add props to alt and localy styles

--- src/components/atoms/Button-icon/index.js
+++ src/components/atoms/Button-icon/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import loginIcon from './login-icon.svg'
+import loginIcon from '_images/login-icon.svg'
+
 import styles from './styles.css'
 
-const ButtonIcon = props => <img className="buttonIcon" src={loginIcon} alt="button-icon" />
+const ButtonIcon = ({ text }) => <img className={styles.buttonIcon} src={loginIcon} alt={text} />
+
+ButtonIcon.propTypes = {
+  text: PropTypes.string,
+}
+
+ButtonIcon.defaultProps = {
+  text: undefined,
+}
 
 export default ButtonIcon
