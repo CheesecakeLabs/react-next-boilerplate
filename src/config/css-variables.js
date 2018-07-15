f2b4338f4927028decafe82c486e5206e730995f
beatriz <beatriz@ckl.io>
2018-07-15 20:02:55 -0300

    Add postcss variables to be able to use css variables into a static representation

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -2,4 +2,23 @@ const styles = {}
 
 styles['font-default'] = "'Source Sans Pro', sans-serif"
 
+styles['color-golden-orange'] = '#fbb03a'
+styles['color-ufo-green'] = '#2ecc71'
+
+styles['color-sand-red'] = '#e74c3c'
+
+styles['color-ghost-white'] = '#f9fbff'
+
+styles['color-concrete-gray'] = '#7f8c8d'
+styles['color-mercury-gray'] = '#e2e0e0'
+styles['color-dusty-gray'] = '#9b9b9b'
+
+styles['color-pacific-blue'] = '#375fa0'
+styles['color-astronaut-blue'] = '#294778'
+styles['color-havelock-blue'] = '#4a90e2'
+styles['color-melrose-blue'] = '#aeC9fd'
+styles['color-alice-blue'] = '#eff4ff'
+styles['color-pale-blue'] = '#adc8ff'
+styles['color-ink-blue'] = '#172b4d'
+
 module.exports = styles
