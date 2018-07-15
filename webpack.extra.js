e6d1753071811e09ce21257edfcd94c31faa2d9f
beatriz <beatriz@ckl.io>
2018-07-15 20:31:55 -0300

    Config post css variables

--- webpack.extra.js
+++ webpack.extra.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   resolve: {
     modules: [path.join(__dirname), path.join(__dirname, 'node_modules')],
-    extensions: ['.js'],
+    extensions: ['.js', '.css'],
     alias: {
       _atoms: path.resolve(__dirname, 'src', 'components', 'atoms'),
       _molecules: path.resolve(__dirname, 'src', 'components', 'molecules'),
