d6e770b6d446d7b29114a6238eb9ebb2f5ca9d2f
beatriz <beatriz@ckl.io>
2018-07-16 17:36:13 -0300

    Install classnames and apply this as a prop on button component

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames/bind'
 
 import Icon from '_atoms/icon'
 
 import styles from './styles.css'
 
-const Button = ({ onClick, iconURL, children }) => (
-  <button className={styles.button} onClick={onClick}>
+const Button = ({ onClick, iconURL, className, children }) => (
+  <button className={classNames(styles.button, className)} onClick={onClick}>
     {iconURL && <Icon src={iconURL} text="button icon" />}
     <p className={styles.buttonText}>{children}</p>
   </button>
@@ -15,11 +16,13 @@ const Button = ({ onClick, iconURL, children }) => (
 Button.propTypes = {
   onClick: PropTypes.func,
   iconURL: PropTypes.string,
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
 }
 
 Button.defaultProps = {
   iconURL: undefined,
+  className: undefined,
   onClick: () => {},
 }
 export default Button
