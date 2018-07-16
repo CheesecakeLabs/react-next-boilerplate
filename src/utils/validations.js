99d40e466805a5a9954cf882d5608ea7071dc26d
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-16 14:03:22 -0300

    feat(validation): Add validation function :tada:

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-useless-escape */
+
 export default {
   required: {
     rule: /[^\s]+/,
-    message: 'This field is required'
+    message: 'This field is required',
   },
   email: {
     rule: /^([\w_+.]+)@[\w_]+\.\w{2,}(?:\.\w{2})?$/,
-    message: 'Invalid email format'
+    message: 'Invalid email format',
   },
   url: {
     rule: /[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/,
-    message: 'Invalid URL format'
-  }
+    message: 'Invalid URL format',
+  },
 }
