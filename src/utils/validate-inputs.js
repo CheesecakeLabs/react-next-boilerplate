b502976c48f8bd5bae98e6a880499c31fae74d63
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-16 14:37:22 -0300

    fix(validation): Only validate empty fields when field is required

--- src/utils/validate-inputs.js
+++ src/utils/validate-inputs.js
@@ -4,6 +4,8 @@ export default function validateRules(selectedValidations, ...newValidations) {
   const reqValidations = { ...validations, ...newValidations }
 
   return function validate(value) {
+    if (!value && selectedValidations.indexOf('required') < 0) return undefined
+
     const firstError = selectedValidations.find(
       validation => !reqValidations[validation].rule.test(value)
     )
