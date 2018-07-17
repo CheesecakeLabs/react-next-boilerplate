4a447a058b5b63d21c2f3dfee6379d8002da9c3c
beatriz <beatriz@ckl.io>
2018-07-17 19:24:06 -0300

    Add stories for editable image component

--- src/components/organisms/editable-image/index.stories.js
+++ src/components/organisms/editable-image/index.stories.js
@@ -5,5 +5,19 @@ import { action } from '@storybook/addon-actions'
 import EditableImage from '_organisms/editable-image'
 
 storiesOf('Organisms/Update avatar', module)
-  .add('default', () => <EditableImage />)
-  .add('with crop', () => <EditableImage withCrop />)
+  .add('from camera or computer', () => <EditableImage />)
+  .add('from computer', () => <EditableImage userMediaEnabled={false} />)
+  .add('with crop', () => (
+    <EditableImage
+      withCrop
+      crop={{
+        aspect: 1,
+        x: 10,
+        y: 10,
+        width: 80,
+        height: 80,
+      }}
+    />
+  ))
+  .add('accepted just png', () => <EditableImage acceptedImgExtensions={['.png']} />)
+  .add('without preview', () => <EditableImage withPreview={false} />)
