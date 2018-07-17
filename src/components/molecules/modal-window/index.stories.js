a4ed706075a4e029bd356b4cad7ea52f3e02bb7b
pattyssilveira <pattyssilveira@gmail.com>
2018-07-17 12:52:32 -0300

    Feat: Modal window component

---
+++ src/components/molecules/modal-window/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import loginIcon from '_images/login-icon.svg'
+
+import Modal from './index'
+
+storiesOf('Modal', module).add('Modal', () => <Modal />)
