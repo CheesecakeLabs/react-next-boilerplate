b8615c787928f996349c8aa2b3569f89ecea7f7b
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-18 17:45:49 -0300

    feat(components): Send function as prop to Form component children to verify if submit is valid

--- src/components/organisms/Form/index.js
+++ src/components/organisms/Form/index.js
@@ -1,18 +1,34 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { Children, Component, cloneElement } from 'react'
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isSubmitValid: {},
+    }
+  }
   onSubmit = ev => {
     ev.preventDefault()
     this.props.handleSubmit(ev)
   }
 
+  isSubmitValid = (fieldName, isChildValid = false) => {
+    const childStatus = { [fieldName]: isChildValid }
+    this.setState(oldState => ({ isSubmitValid: { ...oldState.isSubmitValid, childStatus } }))
+  }
+
   render = () => {
     const { className, children } = this.props
+    const formProps = {
+      isSubmitValid: this.isSubmitValid,
+    }
+    const formChildren = Children.map(children, child => cloneElement(child, formProps))
 
     return (
       <form className={className} onSubmit={this.onSubmit}>
-        {children}
+        {formChildren}
         <input type="submit" value="submit" />
       </form>
     )
