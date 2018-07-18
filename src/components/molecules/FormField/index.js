a3fd0424a1b224bc7bdcdf86704955edb8241818
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-18 18:35:49 -0300

    feat(components): Add label to FormField as a prop

--- src/components/molecules/FormField/index.js
+++ src/components/molecules/FormField/index.js
@@ -1,46 +1,95 @@
 import PropTypes from 'prop-types'
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
+import classNames from 'classnames'
+
+import validate from '_utils/validate-inputs'
+import Input from '_atoms/Input'
 
 import styles from './styles.css'
 
-import Input from '_atoms/Input'
+class FormField extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      errorValidation: '',
+    }
+    this.validate = validate({
+      isRequired: props.required,
+      fieldType: props.type,
+      fieldRules: props.validations,
+      customRules: props.customValidations,
+    })
+    this.handleFormValidation = props.handleFormValidation
+  }
 
-const FormField = ({
-  className,
-  type,
-  field,
-  multiline,
-  required,
-  value,
-  validations,
-  errorMessage,
-  handleChange,
-}) => (
-  <Fragment>
-    <Input
-      className={className}
-      type={type}
-      id={field}
-      name={field}
-      multiline={multiline}
-      required={required}
-      value={value}
-      handleChange={handleChange}
-    />
-  {errorMessage && <p className={styles.error}>{errorMessage}</p>}
-  </Fragment>
-)
+  componentDidMount = () => {
+    this.applyValidation(this.props.value)
+  }
 
-const validationTypes = [PropTypes.string, PropTypes.arrayOf(PropTypes.string), PropTypes.object]
+  applyValidation = value => {
+    const validation = this.validate(value)
+
+    this.setState({ errorValidation: validation })
+    this.handleFormValidation(this.props.field, !validation)
+  }
+
+  handleChange = ev => {
+    this.applyValidation(ev.target.value)
+    return this.props.handleChange(ev)
+  }
+
+  render() {
+    const { className, label, type, field, multiline, required, value, errorMessage } = this.props
+    const visibleError = this.state.errorValidation || errorMessage || null
+
+    const labelChildren = required && <span>*</span>
+    const labelProps = { htmlFor: field }
+
+    const labelComponent =
+      label && label.type ? (
+        React.cloneElement(label, labelProps, label.props.children, labelChildren)
+      ) : (
+        <label htmlFor={field}>
+          {label} {labelChildren}
+        </label>
+      )
+
+    return (
+      <div className={classNames(styles.field, className)}>
+        {labelComponent}
+        <Input
+          className={styles.input}
+          type={type}
+          id={field}
+          name={field}
+          multiline={multiline}
+          required={required}
+          value={value}
+          handleChange={this.handleChange}
+        />
+        {visibleError && <p className={styles.error}>{visibleError}</p>}
+      </div>
+    )
+  }
+}
+
+const labelTypes = [PropTypes.string, PropTypes.element]
+const validationTypes = [PropTypes.string, PropTypes.arrayOf(PropTypes.string)]
+const customValidationTypes = {
+  rule: PropTypes.regexp,
+  message: PropTypes.string,
+}
 
 FormField.propTypes = {
   className: PropTypes.string,
+  label: PropTypes.oneOfType(labelTypes),
   type: PropTypes.string,
   field: PropTypes.string.isRequired,
   multiline: PropTypes.bool,
   required: PropTypes.bool,
   value: PropTypes.string,
   validations: PropTypes.oneOfType(validationTypes),
+  customValidations: PropTypes.shape(customValidationTypes),
   errorMessage: PropTypes.string,
   handleChange: PropTypes.func.isRequired,
   handleFormValidation: PropTypes.func,
@@ -48,12 +97,14 @@ FormField.propTypes = {
 
 FormField.defaultProps = {
   className: undefined,
+  label: undefined,
   type: 'text',
   field: null,
   multiline: false,
   required: false,
   value: '',
   validations: [],
+  customValidations: undefined,
   errorMessage: '',
   handleChange: null,
   handleFormValidation: () => {},
