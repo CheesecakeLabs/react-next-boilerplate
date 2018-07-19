ccbff799909ff457393ce4fd4198d037a2dddac8
beatriz <beatriz@ckl.io>
2018-07-19 16:19:44 -0300

    Implement header item on side navigation

--- src/components/atoms/icon/index.js
+++ src/components/atoms/icon/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './styles.css'
 
-const Icon = ({ src }) => <img className={styles.icon} src={src} alt="icon" />
+const Icon = ({ src, className }) => (
+  <img className={classNames(styles.icon, className)} src={src} alt="icon" />
+)
 
 Icon.propTypes = {
   src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+}
+
+Icon.defaultProps = {
+  className: undefined,
 }
 
 export default Icon
--- src/components/organisms/sidenav/index.js
+++ src/components/organisms/sidenav/index.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 import NavigationItems from '_molecules/navigation-items'
 import NavigationItem from '_atoms/navigation-item'
+import IconButton from '_atoms/icon-button'
+import closeIcon from '_images/close.svg'
 
 import styles from './styles.css'
 
 const Sidenav = () => (
   <div className={styles.sidenav}>
     <nav>
+      <div className={styles.closeNavWrapper}>
+        <IconButton
+          iconURL={closeIcon}
+          className={styles.iconButton}
+          classNameIcon={styles.icon}
+          onClick={() => {
+            console.log('close side nav')
+          }}
+        />
+      </div>
       <NavigationItems>
         <NavigationItem link="teste">Resumo</NavigationItem>
         <NavigationItem link="teste">Relatório de vendas</NavigationItem>
