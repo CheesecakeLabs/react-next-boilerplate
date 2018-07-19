70fb5b2edcc2782176456178d10aeff81bef7fdc
beatriz <beatriz@ckl.io>
2018-07-19 16:19:44 -0300

    Implement header item on side navigation

--- src/components/atoms/icon-button/index.js
+++ src/components/atoms/icon-button/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import Icon from '_atoms/icon'
 
-const IconButton = ({ iconURL, onClick }) => (
-  <button onClick={onClick}>
-    <Icon src={iconURL} />
+import styles from './styles.css'
+
+const IconButton = ({ iconURL, onClick, className, classNameIcon }) => (
+  <button className={classNames(className, styles.iconButton)} onClick={onClick}>
+    <Icon className={classNameIcon} src={iconURL} />
   </button>
 )
 
 Icon.propTypes = {
   iconURL: PropTypes.string.isRequired,
   onClick: PropTypes.func,
+  className: PropTypes.string,
+  classNameIcon: PropTypes.string,
 }
 
 Icon.defaultProps = {
   onClick: () => {},
+  className: undefined,
+  classNameIcon: undefined,
 }
 
 export default IconButton
