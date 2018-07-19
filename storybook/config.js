28f5b098776d6567d7b35bbc6d2f345ff4f6b821
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-19 16:29:10 -0300

    feat(storybook): Add addon to document proptypes and default values of props

--- storybook/config.js
+++ storybook/config.js
@@ -1,6 +1,13 @@
 import { configure } from '@storybook/react'
+import { setDefaults } from '@storybook/addon-info'
 
 const stories = require.context('../src/', true, /stories\.js$/)
+
+setDefaults({
+  header: false,
+  inline: true,
+  source: true,
+})
 
 function loadStories() {
   stories.keys().forEach(stories)
