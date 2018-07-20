ff376e3658a94a35965176ce6c952b60a590fd5e
pattyssilveira <pattyssilveira@gmail.com>
2018-07-20 11:49:57 -0300

    Feat: Modal footer

---
+++ src/components/atoms/modal-footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames/bind'
+
+import styles from './styles.css'
+
+const ModalFooter = ({ className, children }) => (
+  <div className={classNames(styles.modalFooter, className)}>
+    <div className={classNames(styles.modalText, className)}>{children}</div>
+  </div>
+)
+
+ModalFooter.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}
+
+ModalFooter.defaultProps = {
+  className: undefined,
+}
+export default ModalFooter
