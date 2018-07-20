38828a05e2f366fee49e2875cd524e486a89b773
pattyssilveira <pattyssilveira@gmail.com>
2018-07-20 11:50:20 -0300

    Feat: Modal header

---
+++ src/components/atoms/modal-header/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames/bind'
+
+// import CloseIcon from '_atoms/close-icon'
+
+import styles from './styles.css'
+
+const ModalHeader = ({ className, children, closeIcon }) => (
+  <div className={classNames(styles.modalHeader, className)}>
+    <h1 className={classNames(styles.modalTitle, className)}>{children}</h1>
+    {closeIcon}
+  </div>
+)
+
+ModalHeader.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  closeIcon: PropTypes.node,
+}
+
+ModalHeader.defaultProps = {
+  closeIcon: undefined,
+  className: undefined,
+}
+export default ModalHeader
