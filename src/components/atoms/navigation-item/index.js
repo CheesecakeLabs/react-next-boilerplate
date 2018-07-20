8373997f54767f068c0f43cbaa7a841917769a74
beatriz <beatriz@ckl.io>
2018-07-20 10:27:16 -0300

    Add icon on navigation items

--- src/components/atoms/navigation-item/index.js
+++ src/components/atoms/navigation-item/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Icon from '_atoms/icon'
+
 import styles from './styles.css'
 
-const NavigationItem = ({ link, children }) => (
+const NavigationItem = ({ icon, link, children }) => (
   <li className={styles.navItem}>
     <a className={styles.link} href={link}>
-      {children}
+      {icon ? <Icon className={styles.icon} src={icon} /> : undefined}
+      <span>{children}</span>
     </a>
   </li>
 )
 
 NavigationItem.propTypes = {
+  icon: PropTypes.string,
   link: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+}
+
+NavigationItem.defaultProps = {
+  icon: undefined,
 }
 
 export default NavigationItem
