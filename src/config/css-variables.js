6d3772ad5b11e7b1645e6c7108947099b2f39836
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-20 11:37:29 -0300

    style(config): Add font tags to next and storybook config files

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -1,5 +1,6 @@
 const styles = {}
 
 styles['font-default'] = "'Source Sans Pro', sans-serif"
+styles['font-header'] = "'Poppins', sans-serif"
 
 module.exports = styles
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,15 @@ export default class DefaultDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins:300,400,700"
+            rel="stylesheet"
+          />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
