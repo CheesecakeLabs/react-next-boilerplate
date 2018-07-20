a62eab208149eac9c134f4b74313ea5bb0ef73d7
pattyssilveira <pattyssilveira@gmail.com>
2018-07-20 11:49:18 -0300

    Feat: modal window

---
+++ src/components/atoms/modal/index.stories.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import ModalBody from '_atoms/modal-body'
+import loginIcon from '_images/login-icon.svg'
+import CloseIcon from '_atoms/close-icon'
+import ModalHeader from '_atoms/modal-header'
+import ModalFooter from '_atoms/modal-footer'
+import Button from '_atoms/button'
+
+import Modal from './index'
+
+storiesOf('Modal', module).add('Modal', () => (
+  <Modal>
+    <ModalHeader>Happy Text</ModalHeader>
+    <ModalBody>
+      We'll have a super time. Sometimes you learn more from your mistakes than you do from your
+      masterpieces.
+    </ModalBody>
+    <ModalFooter>
+      <Button iconURL={loginIcon} isBlock>
+        Go Crazy
+      </Button>
+    </ModalFooter>
+  </Modal>
+))
+
+storiesOf('Modal', module).add('Modal with Two Buttons', () => (
+  <Modal>
+    <ModalHeader>Happy Text</ModalHeader>
+    <ModalBody>
+      We'll have a super time. Sometimes you learn more from your mistakes than you do from your
+      masterpieces.
+    </ModalBody>
+    <ModalFooter>
+      <Button iconURL={loginIcon} isBlock>
+        Go Crazy
+      </Button>
+      <Button iconURL={loginIcon} isBlock>
+        Go Crazy
+      </Button>
+    </ModalFooter>
+  </Modal>
+))
+
+storiesOf('Modal', module).add('Modal with an X', () => (
+  <Modal>
+    <ModalHeader closeIcon={<CloseIcon />}>Super Happy Text</ModalHeader>
+    <ModalBody>
+      We'll have a super time. Sometimes you learn more from your mistakes than you do from your
+      masterpieces.
+    </ModalBody>
+    <ModalFooter>
+      <Button iconURL={loginIcon} isBlock>
+        Go Crazy
+      </Button>
+    </ModalFooter>
+  </Modal>
+))
