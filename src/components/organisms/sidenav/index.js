667d802887f8192dc41ef659122cb71c123e29e8
beatriz <beatriz@ckl.io>
2018-07-20 15:28:22 -0300

    Adjusts navigation according screen size

--- src/components/organisms/sidenav/index.js
+++ src/components/organisms/sidenav/index.js
@@ -6,43 +6,53 @@ import NavigationItems from '_molecules/navigation-items'
 import NavigationItem from '_atoms/navigation-item'
 import IconButton from '_atoms/icon-button'
 import closeIcon from '_images/close.svg'
+import menuIcon from '_images/menu.svg'
 import settingsIcon from '_images/settings.png'
 
 import styles from './styles.css'
 
-const Sidenav = ({ isOpen, onCloseClick }) => (
-  <div
-    className={classNames(
-      styles.sidenav,
-      { [styles.opened]: isOpen },
-      { [styles.closed]: !isOpen }
-    )}
-  >
-    <nav>
-      <div className={styles.closeNavWrapper}>
-        <IconButton
-          iconURL={closeIcon}
-          className={styles.iconButton}
-          classNameIcon={styles.icon}
-          onClick={onCloseClick}
-        />
-      </div>
-      <NavigationItems>
-        <NavigationItem link="teste" icon={settingsIcon}>
-          Resumo
-        </NavigationItem>
-        <NavigationItem link="teste" icon={settingsIcon}>
-          Relatório de vendas
-        </NavigationItem>
-        <NavigationItem link="teste" icon={settingsIcon}>
-          Relatótio de recebíveis
-        </NavigationItem>
-        <NavigationItem link="teste">Configurações</NavigationItem>
-        <NavigationItem link="teste">Sair</NavigationItem>
-      </NavigationItems>
-    </nav>
-  </div>
-)
+const Sidenav = ({ isOpen, onCloseClick }) => {
+  const headerIcon = iconURL => (
+    <IconButton
+      iconURL={iconURL}
+      className={styles.iconButton}
+      classNameIcon={styles.icon}
+      onClick={onCloseClick}
+    />
+  )
+
+  return (
+    <div
+      className={classNames(
+        styles.sidenav,
+        { [styles.opened]: isOpen },
+        { [styles.closed]: !isOpen }
+      )}
+    >
+      <nav>
+        <div className={styles.navHeaderMobileOnly}>{headerIcon(closeIcon)}</div>
+        <div className={styles.navHeaderDesktopOnly}>{headerIcon(menuIcon)}</div>
+        <NavigationItems>
+          <NavigationItem link="teste" icon={settingsIcon} style={styles.menuItemStyle}>
+            Resumo
+          </NavigationItem>
+          <NavigationItem link="teste" icon={settingsIcon} style={styles.menuItemStyle}>
+            Relatório de vendas
+          </NavigationItem>
+          <NavigationItem link="teste" icon={settingsIcon} style={styles.menuItemStyle}>
+            Relatótio de recebíveis
+          </NavigationItem>
+          <NavigationItem link="teste" icon={settingsIcon} style={styles.menuItemStyle}>
+            Configurações
+          </NavigationItem>
+          <NavigationItem link="teste" icon={settingsIcon} style={styles.menuItemStyle}>
+            Sair
+          </NavigationItem>
+        </NavigationItems>
+      </nav>
+    </div>
+  )
+}
 
 Sidenav.propTypes = {
   isOpen: PropTypes.bool,
