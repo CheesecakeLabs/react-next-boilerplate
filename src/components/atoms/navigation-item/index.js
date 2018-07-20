2613c4f210b0a4c6f8ed61356f27fa47bde07d88
beatriz <beatriz@ckl.io>
2018-07-20 15:31:08 -0300

    Change icon to be a required property on navigation-item component

--- src/components/atoms/navigation-item/index.js
+++ src/components/atoms/navigation-item/index.js
@@ -8,20 +8,22 @@ import styles from './styles.css'
 const NavigationItem = ({ icon, link, style, children }) => (
   <li className={styles.navItem}>
     <a className={styles.link} href={link}>
-      {icon ? <Icon className={styles.icon} src={icon} /> : undefined}
+      <Icon className={styles.icon} src={icon} />
       <span className={style}>{children}</span>
     </a>
   </li>
 )
 
 NavigationItem.propTypes = {
-  icon: PropTypes.string,
+  icon: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  style: PropTypes.string,
 }
 
 NavigationItem.defaultProps = {
   icon: undefined,
+  style: undefined,
 }
 
 export default NavigationItem
