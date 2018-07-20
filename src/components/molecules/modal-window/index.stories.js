bee3f6cabaac77089f462f752fa23f77a54f4266
pattyssilveira <pattyssilveira@gmail.com>
2018-07-20 11:53:38 -0300

    Delete: unused components

--- src/components/molecules/modal-window/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import loginIcon from '_images/login-icon.svg'
-
-import Modal from './index'
-
-storiesOf('Modal', module).add('Modal', () => <Modal />)
