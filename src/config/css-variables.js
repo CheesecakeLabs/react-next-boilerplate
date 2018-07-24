ead0d46f3cb5094059fc1d76be1667bb956f6e8b
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-24 17:06:59 -0300

    style(css variables): Add variables to use color variations in form components

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -2,5 +2,8 @@ const styles = {}
 
 styles['font-default'] = "'Source Sans Pro', sans-serif"
 styles['font-header'] = "'Poppins', sans-serif"
+styles['color-blue'] = '#60cae5'
+styles['color-focusable-blue'] = '#91d0d9'
+styles['color-arrow-blue'] = '#0157ff'
 
 module.exports = styles
