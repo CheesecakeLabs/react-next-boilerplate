de1eb3f30787b76d4bf7c821cb0d47ba267c3ab1
Daniel Leite de Oliveira <dleitee@gmail.com>
2018-07-25 17:17:29 -0300

    refactor(webpack): let all alias in only one file.

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -24,15 +24,6 @@ module.exports = (baseConfig, env, defaultConfig) => {
     ],
   }
   const config = defaultConfig
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    _atoms: path.resolve(__dirname, '..', 'src', 'components', 'atoms'),
-    _molecules: path.resolve(__dirname, '..', 'src', 'components', 'molecules'),
-    _organisms: path.resolve(__dirname, '..', 'src', 'components', 'organisms'),
-    _images: path.resolve(__dirname, '..', 'src', 'images'),
-    _pages: path.resolve(__dirname, '..', 'src', 'pages'),
-    _utils: path.resolve(__dirname, '..', 'src', 'utils'),
-  }
   config.module.rules = config.module.rules.map(rule => (rule.test.test('.css') ? cssRule : rule))
   return config
 }
--- webpack.extra.js
+++ webpack.extra.js
@@ -2,14 +2,15 @@ const path = require('path')
 
 module.exports = {
   resolve: {
-    modules: [path.join(__dirname), path.join(__dirname, 'node_modules')],
+    modules: [path.join(process.cwd()), path.join(process.cwd(), 'node_modules')],
     extensions: ['.js', '.css'],
     alias: {
-      _atoms: path.resolve(__dirname, 'src', 'components', 'atoms'),
-      _molecules: path.resolve(__dirname, 'src', 'components', 'molecules'),
-      _organisms: path.resolve(__dirname, 'src', 'components', 'organisms'),
-      _images: path.resolve(__dirname, 'src', 'images'),
-      _pages: path.resolve(__dirname, 'src', 'pages'),
+      _atoms: path.resolve(process.cwd(), 'src', 'components', 'atoms'),
+      _molecules: path.resolve(process.cwd(), 'src', 'components', 'molecules'),
+      _organisms: path.resolve(process.cwd(), 'src', 'components', 'organisms'),
+      _images: path.resolve(process.cwd(), 'src', 'images'),
+      _utils: path.resolve(process.cwd(), 'src', 'utils'),
+      _pages: path.resolve(process.cwd(), 'src', 'pages'),
     },
   },
 }
