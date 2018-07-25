cb01dd028f56ac1c03d5589e2d06fa1a7824d36e
beatriz <beatriz@ckl.io>
2018-07-25 13:53:14 -0300

    Refactor code on navigation component to receive menu items as children

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react'
 
 import logo from '_components/assets/icons/logo.png'
+import settingsIcon from '_images/settings.png'
+import VerticalNav from '_organisms/vertical-nav'
+import NavigationItems from '_molecules/navigation-items'
+import NavigationItem from '_atoms/navigation-item'
+import cklLogo from '_images/ckl-logo.png'
 
 import styles from './styles.css'
-
-import Button from '_components/atoms/button'
 
 const CKLBoilerplate = () => (
   <Fragment>
@@ -15,7 +18,26 @@ const CKLBoilerplate = () => (
     {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
     <marquee className={styles.text}>React Boilerplate</marquee>
     <hr />
-    <Button>TESTEE</Button>
+
+    <VerticalNav navFooterIcon={cklLogo} navFooterText="2018 Todos os direitos reservados">
+      <NavigationItems>
+        <NavigationItem link="teste" iconSrc={settingsIcon}>
+          Menu 1
+        </NavigationItem>
+        <NavigationItem link="teste" iconSrc={settingsIcon}>
+          Menu 2
+        </NavigationItem>
+        <NavigationItem link="teste" iconSrc={settingsIcon}>
+          Menu 3
+        </NavigationItem>
+        <NavigationItem link="teste" iconSrc={settingsIcon}>
+          Menu 4
+        </NavigationItem>
+        <NavigationItem link="teste" iconSrc={settingsIcon}>
+          Menu 5
+        </NavigationItem>
+      </NavigationItems>
+    </VerticalNav>
   </Fragment>
 )
 
