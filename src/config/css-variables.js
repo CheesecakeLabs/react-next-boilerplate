083e32af46d06ab15df4af4f26a285d6edc561a1
pattyssilveira <pattyssilveira@gmail.com>
2018-07-25 12:12:29 -0300

    style:  add and change colors and font to variables

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -1,5 +1,16 @@
 const styles = {}
 
 styles['font-default'] = "'Source Sans Pro', sans-serif"
+styles['font-header'] = "'Poppins', sans-serif"
+
+styles['color-primary'] = '#39609E'
+styles['color-primary-light'] = '#9ac0ff'
+styles['color-primary-dark'] = '#2B4876'
+styles['color-secondary'] = '#adc8ff'
+styles['color-light-gray'] = '#666666'
+styles['color-white'] = '#ffff'
+styles['color-black'] = '#000000'
+styles['color-light-blue'] = '#007aff'
+styles['color-dark-blue'] = '#005ec4'
 
 module.exports = styles
