d396384b0b72a8dd41e7acfe77d92d13db6f1cc8
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-25 16:21:43 -0300

    style(css variables): Add a color variable

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -5,5 +5,6 @@ styles['font-header'] = "'Poppins', sans-serif"
 styles['color-blue'] = '#60cae5'
 styles['color-focusable-blue'] = '#91d0d9'
 styles['color-arrow-blue'] = '#0157ff'
+styles['color-grey'] = '#979797'
 
 module.exports = styles
