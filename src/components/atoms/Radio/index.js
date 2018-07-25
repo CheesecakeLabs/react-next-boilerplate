f0fda15d141d9737753ccc64cf876b33177dbe18
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-24 21:07:14 -0300

    feat(components): Add Radio component

---
+++ src/components/atoms/Radio/index.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import classNames from 'classnames'
+
+import Input from '_atoms/Input'
+
+import styles from './styles.css'
+
+const Radio = props => {
+  const { className, currentValue, value, handleChange } = props
+  const isChecked = currentValue === value
+  const inputClasses = classNames({
+    [className]: true,
+    [styles.input]: true,
+    [styles.checked]: isChecked,
+  })
+
+  return (
+    <label className={styles.radio} role="radio" aria-checked={isChecked}>
+      <Input {...props} type="radio" className={inputClasses} checked={isChecked} />
+    </label>
+  )
+}
+
+Radio.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  currentValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  handleChange: PropTypes.func.isRequired,
+}
+
+Radio.defaultProps = {
+  className: undefined,
+  id: null,
+  currentValue: null,
+  value: null,
+}
+
+export default Radio
