6c20312b654169594fbd7bc1fd890e5a1e3b3581
beatriz <beatriz@ckl.io>
2018-07-25 17:07:12 -0300

    Rename component files

--- src/components/organisms/drawer/index.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-import Drawer from '_organisms/drawer'
-
-storiesOf('Organisms/Drawer', module).add('Drawer', () => (
-  <Drawer onCloseClick={this.sideMenuHandler} isOpen />
-))
