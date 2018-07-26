ad72b2b62a2f5c99a7124b4cece87565476c0d65
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-26 14:21:02 -0300

    fix(storybook): Show stories inside /components path

--- storybook/config.js
+++ storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react'
 import { setDefaults } from '@storybook/addon-info'
 
-const stories = require.context('../src/', true, /stories\.js$/)
+const stories = require.context('../src', true, /stories\.js$/)
+const storiesComponents = require.context('../components', true, /stories\.js$/)
 
 setDefaults({
   header: false,
@@ -11,6 +12,7 @@ setDefaults({
 
 function loadStories() {
   stories.keys().forEach(stories)
+  storiesComponents.keys().forEach(storiesComponents)
 }
 
 configure(loadStories, module)
