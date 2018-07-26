f988ca086286ba134ae6ea079cfc3cb37fd92e2e
beatriz <beatriz1304@gmail.com>
2018-07-26 17:32:46 -0300

    Add stories for button component and create a style just for storybook

--- components/atoms/button/index.stories.js
+++ components/atoms/button/index.stories.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import loginIcon from '_storybook/assets/images/login-icon.svg'
-
 import Button from '_components/atoms/button'
+import Icon from '_components/atoms/icon'
+import styles from '_storybook/styles.css'
 
-storiesOf('Button', module).add('with text', () => (
-  <Button onClick={() => console.info('test')}>Login</Button>
-))
-
-storiesOf('Button', module).add('with icon', () => (
-  <Button iconURL={loginIcon} onClick={() => console.info('test')}>
-    Login
-  </Button>
-))
-
-storiesOf('Button', module).add('is disabled', () => <Button isDisabled>Login</Button>)
+storiesOf('Atoms/Button', module)
+  .add('with text', () => <Button onClick={() => console.info('test')}>Login</Button>)
+  .add('with icon', () => (
+    <Button
+      iconURL={loginIcon}
+      onClick={() => console.info('test')}
+      className={styles.buttonIconStyle}
+    >
+      <Icon src={loginIcon} alt="login" className={styles.iconStyle} />
+      Login
+    </Button>
+  ))
+  .add('disabled', () => <Button disabled>Login</Button>)
+  .add('customized', () => <Button className={styles.customButton}>Custom</Button>)
