6d646ac24a3e17be3e4d19337c2950a4878e2e5e
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-27 11:23:11 -0300

    feat(components): Move atoms and molecules to /components

--- components/molecules/radio/index.js
+++ components/molecules/radio/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import classNames from 'classnames'
 
-import Input from '_atoms/Input'
+import Input from '_components/atoms/input'
 
 import styles from './styles.css'
 
