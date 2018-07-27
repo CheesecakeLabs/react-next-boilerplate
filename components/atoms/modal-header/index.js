8196ad402638213f23e40235f2df8506a34be503
beatriz <beatriz1304@gmail.com>
2018-07-27 16:45:15 -0300

    Refactor modal-header component

--- components/atoms/modal-header/index.js
+++ components/atoms/modal-header/index.js
@@ -4,21 +4,16 @@ import classNames from 'classnames'
 
 import styles from './styles.css'
 
-const ModalHeader = ({ className, children, closeIcon }) => (
-  <div className={classNames(styles.modalHeader, className)}>
-    <h1 className={classNames(styles.modalTitle, className)}>{children}</h1>
-    {closeIcon}
-  </div>
+const ModalHeader = ({ className, children }) => (
+  <h3 className={classNames(styles.title, className)}>{children}</h3>
 )
 
 ModalHeader.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
-  closeIcon: PropTypes.node,
 }
 
 ModalHeader.defaultProps = {
-  closeIcon: undefined,
   className: undefined,
 }
 export default ModalHeader
