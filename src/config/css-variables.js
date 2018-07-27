504d13c16e8ff5b7bd7c2bbabe992e88d8a89c8c
beatriz <beatriz1304@gmail.com>
2018-07-27 10:30:28 -0300

    Use variables for font and color

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -3,4 +3,9 @@ const styles = {}
 styles['font-default'] = "'Source Sans Pro', sans-serif"
 styles['font-header'] = "'Poppins', sans-serif"
 
+styles['color-primary'] = '#0157ff'
+styles['color-primary-dark'] = '#005ec4'
+
+styles['color-disabled'] = '#C4CDD5'
+
 module.exports = styles
