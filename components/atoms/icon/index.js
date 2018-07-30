b4345e931d0091dca58f5afd55017a2a7d46efd4
beatriz <beatriz1304@gmail.com>
2018-07-30 11:14:04 -0300

    Add className property for icon component to be able to customize this component

--- components/atoms/icon/index.js
+++ components/atoms/icon/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './styles.css'
 
-const Icon = ({ src, text }) => <img className={styles.icon} src={src} alt={text} />
+const Icon = ({ src, alt, className }) => (
+  <img className={classNames(styles.icon, className)} src={src} alt={alt} />
+)
 
 Icon.propTypes = {
   src: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
 
 Icon.defaultProps = {
-  text: undefined,
+  className: undefined,
 }
 
 export default Icon
