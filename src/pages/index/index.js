945b7af4c47b73644384dafaa18d8885aef71875
beatriz <beatriz1304@gmail.com>
2018-07-30 18:43:13 -0300

    Remove vertical nav component from index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,11 +2,6 @@ import React, { Fragment } from 'react'
 
 import Button from '_components/atoms/button'
 import logo from '_components/assets/icons/logo.png'
-import settingsIcon from '_images/settings.png'
-import VerticalNav from '_organisms/vertical-nav'
-import NavigationItems from '_molecules/navigation-items'
-import NavigationItem from '_atoms/navigation-item'
-import cklLogo from '_images/ckl-logo.png'
 
 import styles from './styles.css'
 
@@ -19,26 +14,6 @@ const CKLBoilerplate = () => (
     {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
     <marquee className={styles.text}>React Boilerplate</marquee>
     <hr />
-
-    <VerticalNav navFooterIcon={cklLogo} navFooterText="2018 Todos os direitos reservados">
-      <NavigationItems>
-        <NavigationItem link="teste" iconSrc={settingsIcon}>
-          Menu 1
-        </NavigationItem>
-        <NavigationItem link="teste" iconSrc={settingsIcon}>
-          Menu 2
-        </NavigationItem>
-        <NavigationItem link="teste" iconSrc={settingsIcon}>
-          Menu 3
-        </NavigationItem>
-        <NavigationItem link="teste" iconSrc={settingsIcon}>
-          Menu 4
-        </NavigationItem>
-        <NavigationItem link="teste" iconSrc={settingsIcon}>
-          Menu 5
-        </NavigationItem>
-      </NavigationItems>
-    </VerticalNav>
   </Fragment>
 )
 
