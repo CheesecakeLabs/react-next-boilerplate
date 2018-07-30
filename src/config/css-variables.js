420be591ada04c7f9ed6818040240d72a64c6983
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-30 16:15:19 -0300

    style(css variables): Use only css variables to set colors

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -4,9 +4,7 @@ styles['font-default'] = "'Source Sans Pro', sans-serif"
 styles['font-header'] = "'Poppins', sans-serif"
 
 styles['color-text'] = '#000'
-
-styles['color-blue'] = '#60cae5'
-styles['color-arrow-blue'] = '#0157ff'
+styles['color-light'] = '#FFF'
 
 styles['color-primary'] = '#0157ff'
 styles['color-primary-dark'] = '#005ec4'
@@ -15,6 +13,8 @@ styles['color-input-text'] = '#4c4c4c'
 styles['color-input-border'] = '#979797'
 styles['color-input-focus'] = '#91d0d9'
 styles['color-input-hover'] = styles['color-text']
+styles['color-input-shadow'] = '#91d0d9'
+styles['color-input-primary'] = '#60cae5'
 
 styles['color-disabled'] = '#C4CDD5'
 
