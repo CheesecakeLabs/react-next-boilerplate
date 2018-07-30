902b87c6f982b9ae69b76a050c7288d7abd564fd
Mibs Balsante <mibsbalsante@ckl.io>
2018-07-30 11:19:08 -0300

    style(css variables): Rename color variables to generic names

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -2,13 +2,19 @@ const styles = {}
 
 styles['font-default'] = "'Source Sans Pro', sans-serif"
 styles['font-header'] = "'Poppins', sans-serif"
+
+styles['color-text'] = '#000'
+
 styles['color-blue'] = '#60cae5'
-styles['color-focusable-blue'] = '#91d0d9'
 styles['color-arrow-blue'] = '#0157ff'
-styles['color-grey'] = '#979797'
 
 styles['color-primary'] = '#0157ff'
 styles['color-primary-dark'] = '#005ec4'
+
+styles['color-input-text'] = '#4c4c4c'
+styles['color-input-border'] = '#979797'
+styles['color-input-focus'] = '#91d0d9'
+styles['color-input-hover'] = styles['color-text']
 
 styles['color-disabled'] = '#C4CDD5'
 
