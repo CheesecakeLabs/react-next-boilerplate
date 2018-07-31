4abb12d498ec6012c8a5bf1d0717a69c205e5d3e
beatriz <beatriz1304@gmail.com>
2018-07-31 19:48:24 -0300

    Create a separated nav footer component

--- components/organisms/vertical-nav/drawer.js
+++ components/organisms/vertical-nav/drawer.js
@@ -3,14 +3,12 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 
 import IconButton from '_components/atoms/icon-button'
-import Icon from '_components/atoms/icon'
 import menuIcon from '_components/assets/icons/menu.png'
 import closeIcon from '_components/assets/icons/close.png'
 
-import NavFooter from './footer'
 import styles from './styles.css'
 
-const Drawer = ({ isOpen, onToggleClick, children, footerIcon, footerText, className }) => {
+const Drawer = ({ isOpen, onToggleClick, children, className }) => {
   const headerIcon = iconURL => (
     <IconButton iconURL={iconURL} className={styles.iconButton} onClick={onToggleClick} />
   )
@@ -28,10 +26,6 @@ const Drawer = ({ isOpen, onToggleClick, children, footerIcon, footerText, class
         <div className={styles.navHeaderMobileOnly}>{headerIcon(closeIcon)}</div>
         <div className={styles.navHeaderDesktopOnly}>{headerIcon(menuIcon)}</div>
         {children}
-        <NavFooter className={styles.navFooter}>{footerText}</NavFooter>
-        <NavFooter className={styles.navFooterClosedOnDesktop}>
-          <Icon src={footerIcon} />
-        </NavFooter>
       </nav>
     </div>
   )
@@ -41,16 +35,12 @@ Drawer.propTypes = {
   isOpen: PropTypes.bool,
   onToggleClick: PropTypes.func,
   children: PropTypes.node.isRequired,
-  footerIcon: PropTypes.string,
-  footerText: PropTypes.string,
   className: PropTypes.string,
 }
 
 Drawer.defaultProps = {
   isOpen: false,
   onToggleClick: undefined,
-  footerIcon: undefined,
-  footerText: undefined,
   className: undefined,
 }
 
