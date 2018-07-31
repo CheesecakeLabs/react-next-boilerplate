739116f7e8dbe473ad97feb50d30bbba4e681f55
beatriz <beatriz1304@gmail.com>
2018-07-31 16:28:45 -0300

    Remove unnecessary code

--- components/organisms/leaflet-map/index.stories.js
+++ components/organisms/leaflet-map/index.stories.js
@@ -8,26 +8,24 @@ import { RandomMarkers } from '_utils/random-markers'
 
 storiesOf('Organisms/Map', module)
   .addDecorator(withKnobs)
-  .add('default leaflet map', withInfo({ text: '' })(() => <LeafletMap lat={51.505} lng={-0.09} />))
+  .add('default leaflet map', withInfo()(() => <LeafletMap lat={51.505} lng={-0.09} />))
   .add(
     'leaflet map with markers',
-    withInfo({ text: '' })(() => <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} />)
+    withInfo()(() => <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} />)
   )
   .add(
     'hide layers control of leaflet map',
-    withInfo({ text: '' })(() => (
+    withInfo()(() => (
       <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} showLayersControls={false} />
     ))
   )
   .add(
     'leaflet map with Google layer',
-    withInfo({ text: '' })(() => (
+    withInfo()(() => (
       <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} hasGoogleLayer />
     ))
   )
   .add(
     'mapbox map',
-    withInfo({ text: '' })(() => (
-      <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} showMapbox />
-    ))
+    withInfo()(() => <LeafletMap lat={51.505} lng={-0.09} markers={RandomMarkers()} showMapbox />)
   )
