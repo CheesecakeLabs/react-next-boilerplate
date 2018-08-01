5bc2944120117c1f49aba773710ae9596a14fc2b
pattyssilveira <pattyssilveira@gmail.com>
2018-08-01 15:19:38 -0300

    refactor: change section to accordion.section

--- components/molecules/accordion/index.stories.js
+++ components/molecules/accordion/index.stories.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import Section from '_components/atoms/accordion-section'
 
 import Accordion from './index'
 
 storiesOf('Accordion', module).add('Accordion', () => (
   <Accordion>
-    <Section sectionTitle="Neymar"> O ousado chegou.</Section>
-    <Section sectionTitle="Elon Muskeira">
-      Tesla 4better worldTesla 4better worldTesla 4better worldTesla 4better worldTesla 4better
-      worldTesla 4better world world world world world world world world world world world world
-      world world world world world
-    </Section>
+    <Accordion.Section sectionTitle="Lorem ipsum">
+      Morbi euismod consectetur est, eget vestibulum enim egestas vel. Maecenas ut tortor at massa
+      tincidunt bibendum et vitae diam. Mauris malesuada lorem metus, vitae sollicitudin turpis
+      semper at.
+    </Accordion.Section>
+    <Accordion.Section sectionTitle="Lorem ipsum">
+      Morbi euismod consectetur est, eget vestibulum enim egestas vel. Maecenas ut tortor at massa
+      tincidunt bibendum et vitae diam. Mauris malesuada lorem metus, vitae sollicitudin turpis
+      semper at.
+    </Accordion.Section>
+    <Accordion.Section sectionTitle="Lorem ipsum">
+      Morbi euismod consectetur est, eget vestibulum enim egestas vel. Maecenas ut tortor at massa
+      tincidunt bibendum et vitae diam. Mauris malesuada lorem metus, vitae sollicitudin turpis
+      semper at.
+    </Accordion.Section>
   </Accordion>
 ))
