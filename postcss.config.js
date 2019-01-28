5ea10401ad0fd532dc47595119568f8a77846f85
Mibs Balsante <mibsbalsante@ckl.io>
2019-01-27 22:55:06 -0200

    fix: update postcss-custom-media import to follow current api version

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,16 @@ const postCSSImport = require('postcss-import')()
 const postCSSNested = require('postcss-nested')
 const postCSSCssVariables = require('postcss-css-variables')
 const postCSSCustomMedia = require('postcss-custom-media')({
-  extensions: {
-    '--sm-viewport': '(min-width:320px) and (max-width:640px)',
-    '--md-viewport': '(min-width:640px) and (max-width:960px)',
-    '--lg-viewport': '(min-width:960px)',
-    '--retina-display': '(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi)',
-  },
+  importFrom: [
+    {
+      customMedia: {
+        '--sm-viewport': '(min-width:320px) and (max-width:640px)',
+        '--md-viewport': '(min-width:640px) and (max-width:960px)',
+        '--lg-viewport': '(min-width:960px)',
+        '--retina-display': '(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi)',
+      },
+    },
+  ],
 })
 const postCSSInlineSVG = require('postcss-inline-svg')()
 const colorFunction = require('postcss-color-function')
